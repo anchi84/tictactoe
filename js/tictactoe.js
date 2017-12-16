81778b47d936c6b10edc9ea9a039bbe84d49cd3d
anchi84 <anarili@gmail.com>
2017-12-16 22:38:02 +0100

    three is not done

--- js/tictactoe.js
+++ js/tictactoe.js
@@ -1,51 +1,87 @@
 function main() {
-    var isHere = false;
+    var tableMade = false;
     var $table, $tr, $td;
     var count = 1;
-    $('#start').click(function() {
-        if (!isHere) {
-            $table = $("<table>");
-            $('#tbl').append(function() {
-                for (var i = 0; i < 3; i++) {
-                    $tr = $('<tr>');
-                    $table.append($tr);
-                    for (var j = 0; j < 3; j++) {
-                        $td = $('<td>');
-                        $td.attr("id", "cell_" + count);
-                        $td.click(function() {
-                            if (count % 2 == 0) {
-                                $(this).html('O').css('cursor', 'not-allowed');
-                            } else {
-                                $(this).html('X').css('cursor', 'not-allowed');
-                            }
-                            count++;
 
-                            /*if ($td.html() == "X" && $td.next().html() == "X" && $td.next().next().html() == "X") {
-                                $("res").html("Player X win");
-                            } else if ($td.html() == "O" && $td.next().html() == "O" && $td.next().next().html() == "O") {
-                                $("res").html("Player O win");
-                            } else {
-                                $("res").html("No one wins");
+    $('#start').click(function() {
+        if (!tableMade) {
+             tableMade = true;
+        } else {
+            $table.remove();
+            count = 1;
+            tableMade = false;
+        }
+        refresh();
+        $("#res").html("");
+    });
+
+    function refresh(){
+        $table = $("<table>");
+        for (var i = 0; i < 3; i++) {
+            $tr = $('<tr>');
+                $table.append($tr);
+                for (var j = 0; j < 3; j++) {
+                    $td = $('<td>');
+                    $td.attr("id", "cell_" +i +j);
+                    $tr.append($td);
+                    console.log($td.attr("id"));
+                    $td.on('click', function() {
+                        if (count % 2 == 0) {
+                            $(this).html('O').off('click');
+                        } else {
+                            $(this).html('X').off('click');
+                        }
+                        
+                        if(count >= 5 && count < 9) {
+                            if(three("X")) {
+                                $("#res").html("Player X wins");
+                            } else if(three("O")) {
+                                $("#res").html("Player O wins");
+                            }
+                            
+                            /*if($('#cell_00').html() == "X" && $('#cell_01').html() == "X" && $('#cell_02').html() == "X") {
+                                $("#res").html("Player X wins");
+                            } else if($('#cell_00').html() == "O" && $('#cell_01').html() == "O" && $('#cell_02').html() == "O") {
+                                $("#res").html("Player O wins");
                             }*/
-                        });
-                        $tr.append($td);
+
+                        } else if(count == 9){
+                            $("#res").html("No one wins");
+                        }
+
+
+                        count++;   
+                    });
+       
+                }
+
+        }         
+        tableMade = true;
+        $('#tbl').append($table);
+
+        function three(string) {
+            var count1 = 0; 
+            var count2 = 0;
+            for(var i = 0; i < 3; i++)
+                for(var j = 0; j < 3; j++) {
+                    if (i == j && $('#cell_'+i+j).html() == string) {
+                       count1++;
+                    } 
+                    if(i + j == 2 && $('#cell_'+i+j).html() == string) {
+                        count2++;
                     }
                 }
-                isHere = true;
-                return $table;
-            });
+                    
+            if(count1 == 3 || count2 == 3) {
+                $('td').off('click');
+                return true;     
+            }
+                 
         }
-    });
-}
+                                 
 
-/*function refresh() {
-    if ($td.html() == "X" && $td.next().html() == "X" && $td.next().next().html() == "X") {
-        $("res").html("Player X win");
-    } else if ($td.html() == "O" && $td.next().html() == "O" && $td.next().next().html() == "O") {
-        $("res").html("Player O win");
-    } else {
-        $("res").html("No one wins");
     }
-}*/
+
+}
 
 $(document).ready(main);
